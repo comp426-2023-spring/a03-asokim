2749cc595d9e19be4b560f9d0c5267ac979482c3
Anna Kim <asokim@email.unc.edu>
2023-04-12 23:35:21 -0400

    Edit imports for rps/rpsls

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
 import minimist from 'minimist';
-import { playRps } from '../lib/rpsls.js';
+import { Rps } from '../lib/rpsls.js';
 
 const args = minimist(process.argv.slice(2));
--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
 import minimist from 'minimist';
-import { playRpsls } from '../lib/rpsls.js';
+import { Rpsls } from '../lib/rpsls.js';
 
 const args = minimist(process.argv.slice(2));
