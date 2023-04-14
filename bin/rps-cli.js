8c0192bac31a16500b6c926bdba455f2606fb2fd
Anna Kim <asokim@email.unc.edu>
2023-04-14 18:46:07 -0400

    add help args

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 
 import minimist from 'minimist';
-import { Rps } from '../lib/rpsls.js';
+import { rps } from '../lib/rpsls.js';
 
 const args = minimist(process.argv.slice(2));
+
+if (args.h || args.help) {
+    help();
+}
--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
 
 import minimist from 'minimist';
-import { Rpsls } from '../lib/rpsls.js';
+import { rpsls } from '../lib/rpsls.js';
 
 const args = minimist(process.argv.slice(2));
+
+
+
+if (args.h || args.help) {
+    help();
+}
