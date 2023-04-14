715bb9f8c84a45cf5c6bb502df81b4c10b1d927f
Anna Kim <asokim@email.unc.edu>
2023-04-14 18:49:02 -0400

    Added help

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -5,6 +5,15 @@ import { rpsls } from '../lib/rpsls.js';
 
 const args = minimist(process.argv.slice(2));
 
+const help = `Usage: node-rps [SHOT]
+Play Rock Paper Scissors (RPS)
+  -h, --help      display this help message and exit
+  -r, --rules     display the rules and exit
+Examples:
+  node-rps        Return JSON with single player RPS result.
+                  e.g. {"player":"rock"}
+  node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                  e.g {"player":"rock","opponent":"scissors","result":"win"}`;
 
 
 if (args.h || args.help) {
